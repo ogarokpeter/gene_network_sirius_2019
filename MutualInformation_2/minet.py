0ab2b5396c6857dcf4e073ef91ac85fc42d27af4
ogarokpeter <ogarokpeter@gmail.com>
2019-02-06 14:37:47 +0300

    Everything except data generation in one repo

---
+++ MutualInformation_2/minet.py
@@ -0,0 +1,67 @@
+import rpy2
+import rpy2.robjects as ro
+import rpy2.robjects.numpy2ri as rn
+import numpy as np
+import xml.dom.minidom as md
+from sklearn.metrics import roc_curve, auc
+
+datadirname = "/home/user/Sirius/MutualInformation_2/MI/Data"
+datafilename = datadirname + "/{0}/{0}_data.txt"
+graphfilename = datadirname + "/{0}/{0}_graph.xml"
+
+datalist = ['exps_10', 'exps_10_2', 'exps_10_bgr', 'exps_50', 'exps_50_2', 'exps_50_bgr', 'exps_100', 'exps_100_2', 'exps_100_bgr']
+
+def run_minet(filename):
+
+    rn.activate()
+
+    code = """library(minet)
+    filename <- '""" + filename + """'
+    first <- readLines(filename, n=1)
+    names <- strsplit(first, '\t')
+    names <- unlist(names, use.names=FALSE)
+    d <- read.table(filename, skip=1, col.names = names)
+
+    mim <- build.mim(d, estimator = "mi.empirical", disc = "equalfreq")
+
+    weight_adjacency_matrix <- minet(mim, method="mrnetb", estimator="mi.empirical", disc="equalfreq");
+
+    weight_adjacency_matrix;
+    """
+
+    f = ro.r(code)
+
+    weight_adjacency_matrix = np.array(f)
+
+    long_array = weight_adjacency_matrix[np.triu_indices(weight_adjacency_matrix.shape[0])]
+    return long_array
+
+
+
+def xml_graph_to_adjacency_matrix(filename):
+    dom = md.parse(filename)
+
+    # print(dom.toprettyxml())
+
+    nodes = dom.getElementsByTagName("Node")
+    ids = [int(a.getAttribute('id')) for a in nodes]
+    # parserint(ids)
+    adjacency_matrix = np.zeros(shape=(len(ids), len(ids)))
+    edges = dom.getElementsByTagName("Edge")
+    for e in edges:
+        source = int(e.getElementsByTagName('from')[0].firstChild.nodeValue)
+        target = int(e.getElementsByTagName('to')[0].firstChild.nodeValue)
+        adjacency_matrix[source][target] = 1
+        adjacency_matrix[target][source] = 1
+    return adjacency_matrix
+
+for dataname in datalist:
+
+    long_array = run_minet(datafilename.format(dataname))
+    true_matrix = xml_graph_to_adjacency_matrix(graphfilename.format(dataname))
+    true_array = true_matrix[np.triu_indices(true_matrix.shape[0])]
+
+    fpr, tpr, thresholds = roc_curve(true_array, long_array)
+    roc_auc = auc(fpr, tpr)
+
+    print(roc_auc)
